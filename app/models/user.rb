040fef10a2870a3f621b4ca768ad18cc7fae8b99
NUKEDO <nukedo.4444.orz@gmail.com>
2020-10-29 18:16:10 +0900

    Rubocopを実行
    bundle exec rubocop -a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d{6,}]+\z/i.freeze
-  VALID_CHINESE_CHAR = /\A[ぁ-んァ-ン一-龥]+\z/
-  VALID_KATAKANA = /\A[ァ-ン]+\z/
+  VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d{6,}]+\z/i.freeze
+  VALID_CHINESE_CHAR = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  VALID_KATAKANA = /\A[ァ-ン]+\z/.freeze
   validates :password,              presence: true, format: { with: VALID_PASSWORD }
   validates :nickname,              presence: true, length: { maximum: 40 }
   validates :family_name,           presence: true, format: { with: VALID_CHINESE_CHAR }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number:2)}
-    email                 {Faker::Internet.free_email}
-    password              {"123abc"}
-    password_confirmation {password}
-    family_name           {"山田"}
-    first_name            {"太郎"}
-    family_name_katakana  {"ヤマダ"}
-    first_name_katakana   {"タロウ"}
-    birthday              {"1990-10-29"}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { '123abc' }
+    password_confirmation { password }
+    family_name           { '山田' }
+    first_name            { '太郎' }
+    family_name_katakana  { 'ヤマダ' }
+    first_name_katakana   { 'タロウ' }
+    birthday              { '1990-10-29' }
   end
 end
