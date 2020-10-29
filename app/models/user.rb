0503e8d7f187fad0a2d8147e0445cf177b47cf28
NUKEDO <nukedo.4444.orz@gmail.com>
2020-10-29 12:06:51 +0900

    passwordのバリデーションを追記
    英数字混合6文字以上

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d{6,}]+\z/i.freeze
   VALID_CHINESE_CHAR = /\A[ぁ-んァ-ン一-龥]+\z/
   VALID_KATAKANA = /\A[ァ-ン]+\z/
+  validates :password,              presence: true, format: { with: VALID_PASSWORD }
   validates :nickname,              presence: true
   validates :family_name,           presence: true, format: { with: VALID_CHINESE_CHAR }
   validates :first_name,            presence: true, format: { with: VALID_CHINESE_CHAR }
