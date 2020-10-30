a442f26691629e1a724a3d450effd26adce6f088
NUKEDO <nukedo.4444.orz@gmail.com>
2020-10-30 11:29:51 +0900

    バリデーション記述修正
    バリデーション記述が全角カタカナに対応するよう修正
    presence: trueをwith_optionで記述に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,15 @@ class User < ApplicationRecord
 
   VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d{6,}]+\z/i.freeze
   VALID_CHINESE_CHAR = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-  VALID_KATAKANA = /\A[ァ-ン]+\z/.freeze
-  validates :password,              presence: true, format: { with: VALID_PASSWORD }
-  validates :nickname,              presence: true, length: { maximum: 40 }
-  validates :family_name,           presence: true, format: { with: VALID_CHINESE_CHAR }
-  validates :first_name,            presence: true, format: { with: VALID_CHINESE_CHAR }
-  validates :family_name_katakana,  presence: true, format: { with: VALID_KATAKANA }
-  validates :first_name_katakana,   presence: true, format: { with: VALID_KATAKANA }
-  validates :birthday,              presence: true
+  VALID_KATAKANA = /\A[ァ-ヶー－]+\z/.freeze
+
+  with_options presence: true do
+    validates :password,              format: { with: VALID_PASSWORD }
+    validates :nickname,              length: { maximum: 40 }
+    validates :family_name,           format: { with: VALID_CHINESE_CHAR }
+    validates :first_name,            format: { with: VALID_CHINESE_CHAR }
+    validates :family_name_katakana,  format: { with: VALID_KATAKANA }
+    validates :first_name_katakana,   format: { with: VALID_KATAKANA }
+    validates :birthday
+  end
 end
