813efa7acd5c6250f24a90cc064ae4515a2e4d73
NUKEDO <nukedo.4444.orz@gmail.com>
2020-10-30 14:57:27 +0900

    インスタンス作成、ストロングパラメーター記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,12 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :price, :name, :text, :category_id, :detail_id, :burden_id, :shipment_source_id, :number_of_days_id).merge(user_id: current_user.id)
   end
 end
