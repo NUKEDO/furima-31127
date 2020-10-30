5db58e3920d3c38abab0c38ce2213a60fa0e11ff
NUKEDO <nukedo.4444.orz@gmail.com>
2020-10-30 16:09:59 +0900

    ActiveHashを用いた設定
    モデル作成
    記述
    バリデーション修正（_idの削除)
    belongs_to_active_hashのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,23 @@
 class Item < ApplicationRecord
-  with_options precense: true do
+  with_options presence: true do
     validates :price
     validates :name
     validates :text
+    validates :category
+    validates :detail
+    validates :burden
+    validates :shipment_source
+    validates :number_of_days
+  end
+
+  with_options numericality: { other_than: 1 } do
     validates :category_id
-    validates :detail_id
-    validates :burden_id
-    validates :shipment_source_id
-    validates :number_of_days_id
+  
   end
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
 end
