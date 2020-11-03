8fc9d95bed798765cab214832abe153bf6afada8
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-03 12:47:35 +0900

    purchaseテーブル作成、showビュー編集
    purchaseテーブル作成
    createアクション作成
    showページが表示されるよう変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,17 @@
 class OrdersController < ApplicationController
   def show
+    @item = Item.find(params[:id])
+    @purchase_record = Purchase_record.new
+  end
+
+  def create
+    @purchase_record = Purchase_record.new(purchase_record_params)
+
+  end
+
+  private
+
+  def purchase_record_params
+    params.require(:purchase_record).merge(user_id: current_user.id, item_id: current_item.id)
   end
 end
