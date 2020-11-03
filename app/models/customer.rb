f4dd73792ce704b0afe8ea6369558cd5a8e7365d
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-04 08:52:23 +0900

    customerモデル作成
    アソシエーション記述
    マイグレーション記述、db:migrate
    ActiveHash設定
    ビューを元に戻す

---
+++ app/models/customer.rb
@@ -0,0 +1,15 @@
+class Customer < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id,   numericality: { only_integer: true, other_than: 1, greater_than_or_equal_to: 2, less_than_or_equal_to: 48 }
+    validates :city
+    validates :house_number
+    validates :phone_number
+    validates :purchase_record
+  end
+  
+  belongs_to :purchase_record
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture_id
+end
