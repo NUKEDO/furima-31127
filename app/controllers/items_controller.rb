bf5c961c63dc3c25b48960b15be454d3173fe7fd
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-09 15:02:38 +0900

    destroyアクションのルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  only: %i[new edit]
-  before_action :find_item,           only: %i[show edit update]
+  before_action :find_item,           only: %i[show edit update destroy]
 
   def index
     @items = Item.all.order(created_at: 'DESC').includes(:purchase_record)
@@ -31,6 +31,9 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
   end
 
   private
